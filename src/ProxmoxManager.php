4536dbd26ce40dd4a92666638994569dce5b05cf
Tony Roy <tonyroy@wcksoft.com>
2026-05-11 15:56:07 -0300

    Add ProxmoxManager to resolve multiple named Proxmox connections.

---
+++ src/ProxmoxManager.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Irabbi360\Proxmox;
+
+use Illuminate\Contracts\Container\Container;
+
+class ProxmoxManager
+{
+    /** @var Container */
+    protected $app;
+
+    /**
+     * Cached Proxmox client instances per connection name.
+     *
+     * @var array<string, Proxmox>
+     */
+    protected $clients = [];
+
+    public function __construct(Container $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Get a Proxmox client for the given connection name.
+     * If no name is given, the configured default connection is used.
+     */
+    public function connection(?string $name = null): Proxmox
+    {
+        $name = $name ?? $this->getDefaultConnection();
+
+        if (! isset($this->clients[$name])) {
+            $this->clients[$name] = $this->resolveConnection($name);
+        }
+
+        return $this->clients[$name];
+    }
+
+    /**
+     * Magic entrypoint so you can call ProxmoxManager methods statically
+     * via the existing facades while still picking a connection:
+     *
+     * ProxmoxNode::on('site2')->getVMs('pve-node');
+     */
+    public function on(string $name): Proxmox
+    {
+        return $this->connection($name);
+    }
+
+    protected function getDefaultConnection(): string
+    {
+        return (string) $this->app['config']['proxmox.default'] ?? 'default';
+    }
+
+    protected function resolveConnection(string $name): Proxmox
+    {
+        $config = $this->getConnectionConfig($name);
+
+        return new ProxmoxNode(
+            $config['hostname'],
+            $config['username'],
+            $config['password'],
+            $config['realm'] ?? 'pam',
+            (int) ($config['port'] ?? 8006)
+        );
+    }
+
+    protected function getConnectionConfig(string $name): array
+    {
+        $config = $this->app['config']['proxmox'];
+
+        // Prefer the new `connections` array
+        if (isset($config['connections'][$name])) {
+            return $config['connections'][$name];
+        }
+
+        // Fallback to old single-connection keys for backwards compatibility
+        return [
+            'hostname' => $config['hostname'] ?? 'proxmox.example.com',
+            'username' => $config['username'] ?? 'root',
+            'password' => $config['password'] ?? '',
+            'realm' => $config['realm'] ?? 'pam',
+            'port' => $config['port'] ?? 8006,
+            'node' => $config['node'] ?? '',
+        ];
+    }
+}
